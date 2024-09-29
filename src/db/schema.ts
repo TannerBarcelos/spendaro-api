e48ca904b34b6acf7b9a8703d47869c9d55f1f65
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-09-28 17:23:04 -0700

    Refactor database connection setup and plugin registration, add environment variables for database configuration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { timestamp } from "drizzle-orm/pg-core";
 import { date } from "drizzle-orm/pg-core";
 import { integer, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
@@ -19,10 +19,14 @@ export const users = pgTable('users', {
   updated_at: updatedTs,
 });
 
+export const usersRelations = relations(users, ({ many }) => ({
+  budgets: many(budgets),
+}));
+
 // This table is used to store the budgets that belong to a user
 export const budgets = pgTable('budgets', {
   id: serial('id').primaryKey(),
-  user_id: integer('user_id').references(() => users.id),
+  user_id: integer('user_id').references(() => users.id, { onDelete: 'cascade' }), // when a user is deleted, all their budgets should be deleted as well
   budget_name: text('budget_name').notNull(),
   budget_description: text('budget_description').default(''),
   assigned_amount: integer('assigned_amount').default(0),
@@ -30,20 +34,30 @@ export const budgets = pgTable('budgets', {
   updated_at: updatedTs,
 });
 
+export const budgetsRelations = relations(budgets, ({ one, many }) => ({
+  user: one(users),
+  categories: many(budget_categories),
+}));
+
 // This table is used to store the categories that belong to a budget
 export const budget_categories = pgTable('budget_categories', {
   id: serial('id').primaryKey(),
-  budget_id: integer('budget_id').references(() => budgets.id),
+  budget_id: integer('budget_id').references(() => budgets.id, { onDelete: 'cascade' }), // when a budget is deleted, all its categories should be deleted as well
   category_name: text('category_name').notNull(),
   category_description: text('category_description').default(''),
   created_at: createdTs,
   updated_at: updatedTs,
 });
 
+export const budgetCategoriesRelations = relations(budget_categories, ({ one, many }) => ({
+  budget: one(budgets),
+  budget_category_items: many(budget_category_items),
+}));
+
 // This table is used to store the items that belong to a budget category
 export const budget_category_items = pgTable('budget_category_items', {
   id: serial('id').primaryKey(),
-  category_id: integer('category_id').references(() => budget_categories.id),
+  category_id: integer('category_id').references(() => budget_categories.id, { onDelete: 'cascade' }), // when a category is deleted, all its items should be deleted as well
   item_name: text('item_name').notNull(),
   item_description: text('item_description').default(''),
   item_amount: integer('item_amount').default(0),
@@ -51,17 +65,27 @@ export const budget_category_items = pgTable('budget_category_items', {
   updated_at: updatedTs,
 });
 
+export const budgetCategoryItemsRelations = relations(budget_category_items, ({ one, many }) => ({
+  category: one(budget_categories),
+  transactions: many(budget_category_item_transactions), // an item in a category can have N transactions, so we need to establish a one-to-many relationship for N transactions to one item
+}));
+
 // This table is used to store the transactions made and assign them to a budget category item
 export const budget_category_item_transactions = pgTable('budget_category_item_transactions', {
   id: serial('id').primaryKey(),
-  item_id: integer('item_id').references(() => budget_category_items.id),
+  item_id: integer('item_id').references(() => budget_category_items.id, { onDelete: 'cascade' }), // when an item is deleted, all its transactions should be deleted as well
   transaction_amount: integer('transaction_amount').notNull(),
   transaction_date: date('transaction_date').notNull(), // date of the transaction (can be different from the created_at date, so we need to store it separately)
   transaction_description: text('transaction_description').default(''),
-  transaction_type_id: integer('transaction_type_id').references(() => budget_category_item_transaction_types.id),
+  transaction_type_id: integer('transaction_type_id').references(() => budget_category_item_transaction_types.id, { onDelete: 'set null' }), // when a transaction type is deleted, we should set the transaction type id to null since transactions should still persist
   created_at: createdTs,
   updated_at: updatedTs,
 });
+
+export const budgetCategoryItemTransactionsRelations = relations(budget_category_item_transactions, ({ one }) => ({
+  item: one(budget_category_items),
+  transaction_type: one(budget_category_item_transaction_types),
+}));
 
 // This table is used to store the types of transactions that can be made on a budget category item i.e. income, expense, etc. (not using enum as users can add their own transaction types)
 export const budget_category_item_transaction_types = pgTable('budget_category_item_transaction_types', {
