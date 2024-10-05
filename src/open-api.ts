b30e297cf2fe494d3294234e484bf28593b99941
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-10-04 17:28:45 -0700

    Refactor import paths in open-api.ts for better organization

--- src/open-api.ts
+++ src/open-api.ts
@@ -1,21 +1,25 @@
 import { SwaggerOptions } from "@fastify/swagger";
 import { FastifySwaggerUiOptions } from "@fastify/swagger-ui";
 
+// Learn more here -> https://github.com/fastify/fastify-swagger
 export const swaggerConfig: SwaggerOptions = {
   openapi: {
     openapi: '3.0.0',
+    // metadata about the API
     info: {
       title: 'Spendaro REST API',
       version: '1.0.0',
       description:
         'A feature-rich REST API for managing your personal finances on the Spendaro platform',
     },
+    // available servers to run the API methods on from the Swagger UI
     servers: [
       {
         url: 'http://localhost:8010',
         description: 'Local Development Server',
       },
     ],
+    // represent the sections in the UI - these are assigned as tags to each API schema object
     tags: [
       {
         name: 'budget',
