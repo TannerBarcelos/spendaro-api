4c76871166a2aa41b35963bbd9e66696bdb33b65
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-10-08 18:41:47 -0700

    Refactor gitignore and vscode settings

--- src/env.ts
+++ src/env.ts
@@ -13,19 +13,5 @@ export const env = createEnv({
   },
   // eslint-disable-next-line node/no-process-env
   runtimeEnv: process.env,
-
-  /**
-   * By default, this library will feed the environment variables directly to
-   * the Zod validator.
-   *
-   * This means that if you have an empty string for a value that is supposed
-   * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
-   * it as a type mismatch violation. Additionally, if you have an empty string
-   * for a value that is supposed to be a string with a default value (e.g.
-   * `DOMAIN=` in an ".env" file), the default value will never be applied.
-   *
-   * In order to solve these issues, we recommend that all new projects
-   * explicitly specify this option as true.
-   */
   emptyStringAsUndefined: true,
 });
