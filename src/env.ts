b5f56a7745e5ea4fdd03f32d4c9b426ee3c8e294
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-11-02 14:13:44 -0700

    Add dotenv dependency and configure environment variable loading

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,20 @@
 import { createEnv } from "@t3-oss/env-core";
+import dotenv from "dotenv";
 import { z } from "zod";
 
+// load .env file into process.env - leverage t3-env to validate the env vars and make them available
+dotenv.config();
+
 export const env = createEnv({
-  server: { NODE_ENV: z.string(), DB_HOST: z.string(), DB_PORT: z.number(), DB_USER: z.string(), DB_PASSWORD: z.string(), DB_NAME: z.string(), JWT_SECRET: z.string(), UPLOADTHING_TOKEN: z.string() },
+  server: {
+    NODE_ENV: z.string().default("development"),
+    DB_HOST: z.string(),
+    DB_PORT: z.coerce.number(), // .env vars are always strings, so we need to coerce them to numbers
+    DB_USER: z.string(),
+    DB_PASSWORD: z.string(),
+    DB_NAME: z.string(),
+    JWT_SECRET: z.string(),
+    UPLOADTHING_TOKEN: z.string(),
+  },
   runtimeEnv: process.env,
 });
