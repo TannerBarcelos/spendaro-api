6f2bdd362d7469d95c6cd39ce01ee94358a79a8c
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-11-02 13:50:46 -0700

    Refactor type declarations for Fastify and JWT integration

--- global.d.ts
+++ global.d.ts
@@ -6,19 +6,7 @@ import Redis from "ioredis";
 
 import * as schema from "./src/db/schema.ts";
 
-namespace NodeJS {
-  interface ProcessEnv {
-    DB_USER: string;
-    DB_PASSWORD: string;
-    DB_HOST: string;
-    DB_PORT: string;
-    DB_NAME: string;
-    JWT_SECRET: string;
-    COOKIE_SECRET: string;
-    UPLOADTHING_TOKEN: string;
-  }
-}
-
+// declaration merging - add properties to existing types for Fastify
 declare module "fastify" {
   interface FastifyInstance {
     db: PostgresJsDatabase<typeof schema>;
@@ -30,14 +18,12 @@ declare module "fastify" {
   }
 }
 
+// declaration merging - add properties to existing types for JWT
 declare module "@fastify/jwt" {
   interface FastifyJWT {
-    // the type of the payload to be signed (jwt.sign will only accept an object of this type)
     payload: {
       user_id: number;
     };
-
-    // the type of the decoded payload that will be available in the request via the fastify/jwt plugin
     user: {
       user_id: number;
     };
