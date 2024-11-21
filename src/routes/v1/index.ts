cfef54842007c0b96f6fbc325beedd60765a7b89
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-11-21 15:59:33 -0800

    Refactor authentication handling by moving preHandler hook to v1 index and removing it from budget and user routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,22 @@
 import type { FastifyInstance } from "fastify";
 
+import { getAuth } from "@clerk/fastify";
+
+import { ForbiddenError } from "@/utils/error";
+import { clerkClient } from "@/utils/http";
+
 import { budgetRoutes } from "./budget-routes";
 import { userRoutes } from "./user-routes";
 
 export async function v1(server: FastifyInstance) {
+  server.addHook("preHandler", async (request) => {
+    const { userId, sessionClaims } = getAuth(request);
+    if (!userId) {
+      throw new ForbiddenError("Access denied. Authentication required.");
+    }
+    console.log(sessionClaims.metadata);
+    request.user = await clerkClient.users.getUser(userId);
+  });
   await server.register(budgetRoutes, { prefix: "/budgets" });
   await server.register(userRoutes, { prefix: "/user" });
 }
--- src/routes/v1/user-routes.ts
+++ src/routes/v1/user-routes.ts
@@ -1,20 +1,10 @@
 import type { FastifyInstance } from "fastify";
 
-import { clerkClient, getAuth } from "@clerk/fastify";
-
 import { UserHandlers } from "@/handlers/user/user-handlers";
 import { UserRepository } from "@/repositories/user-repository";
 import { UserService } from "@/services/user-service";
-import { ForbiddenError } from "@/utils/error";
 
 export async function userRoutes(instance: FastifyInstance) {
-  instance.addHook("preHandler", async (request, _) => {
-    const { userId } = getAuth(request);
-    if (!userId) {
-      throw new ForbiddenError("Access denied. Authentication required.");
-    }
-    request.user = await clerkClient.users.getUser(userId);
-  });
   const userRepo = new UserRepository(instance.db);
   const userService = new UserService(userRepo);
   const userHandlers = new UserHandlers(userService);
